d3ab93353ab0289f28485a9aa872b07cbcf7ed63
Riley Zurrin <56184444+RileyZurrin@users.noreply.github.com>
2023-12-04 21:00:25 -0500

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -58,7 +59,9 @@ def extract_page(html):
 
 def extract(url):
     # Extraction
-    driver = webdriver.Firefox()
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
     homepage = driver.get(url)
 
     # Initial extraction for the first set of rows
